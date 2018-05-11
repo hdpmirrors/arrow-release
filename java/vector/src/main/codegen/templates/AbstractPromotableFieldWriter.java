3bd1c1f63f13d25637a08c12f3fad110d54a2357
Teddy Choi <pudidic@gmail.com>
2018-05-12 08:04:54 +0900

    BUG-101888: [Java] Add list<decimal> writer
    Change-Id: I76ad232d80d2da250928aca1dd2392e3a28d7b84

--- java/vector/src/main/codegen/templates/AbstractPromotableFieldWriter.java
+++ java/vector/src/main/codegen/templates/AbstractPromotableFieldWriter.java
@@ -16,6 +16,9 @@
  * limitations under the License.
  */
 
+import io.netty.buffer.ArrowBuf;
+import org.apache.arrow.vector.types.Types;
+import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.drill.common.types.TypeProtos.MinorType;
 
 <@pp.dropOutputFile />
